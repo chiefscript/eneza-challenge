2eb3f4b0575d3ac0d97ea88670a692f7b07a86f9
chiefscript <>
2018-05-29 22:31:57 +0300

    Quiz model & migration

---
+++ app/Quiz.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Quiz extends Model
+{
+    use SoftDeletes;
+    
+    protected $fillable = ['question'];
+}
