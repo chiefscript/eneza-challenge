38e6f8591422a562ea95f53dac963e5133eded3e
chiefscript <chiefscript@gmail.com>
2018-05-30 09:17:57 +0300

    Register seeders to be called

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Course;
+use App\Subject;
+use App\Tutorial;
+use App\Quiz;
+use App\Question;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +17,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        Course::truncate();
+        Subject::truncate();
+        Tutorial::truncate();
+        Quiz::truncate();
+        Question::truncate();
+
+         $this->call([
+             CoursesTableSeeder::class,
+             SubjectsTableSeeder::class,
+             TutorialsTableSeeder::class,
+             QuizzesTableSeeder::class,
+             QuestionsTableSeeder::class
+         ]);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
