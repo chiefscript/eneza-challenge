265e9b1f3e7dfbcd6c8483fe830f15838574dc93
chiefscript <chiefscript@gmail.com>
2018-05-30 10:53:14 +0300

    Question CRUD functions

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Question;
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $questions = Question::all();
+        return response()->json($questions);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $question = new Question();
+        $question->question= $request->question;
+        $question->choices = $request->choices;
+
+        $question->save();
+        return response()->json($question);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $question = Question::find($id);
+        return response()->json($question);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $question= Question::find($id);
+
+        $question->question = $request->input('question');
+        $question->choices = $request->input('choices');
+        $question->save();
+        return response()->json($question);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $question = Question::find($id);
+        $question->delete();
+        return response()->json('The question has been deleted successfully');
+    }
+}
