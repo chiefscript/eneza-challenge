288fc2f079e1cdc5b5fa847403dcfa04e077f1eb
chiefscript <chiefscript@gmail.com>
2018-05-30 09:18:20 +0300

    Courses controller

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $courses = Course::all();
+        return response()->json($courses);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $course = new Course();
+        $course->name= $request->name;
+
+        $course->save();
+        return response()->json($course);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $course = Course::find($id);
+        return response()->json($course);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $course= Course::find($id);
+
+        $course->name = $request->input('name');
+        $course->save();
+        return response()->json($course);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $course = Course::find($id);
+        $course->delete();
+        return response()->json('The course has been deleted successfully');
+    }
+}
