187285fca4be468efd46348dff85acf3d7b9b036
chiefscript <chiefscript@gmail.com>
2018-05-30 09:31:46 +0300

    Subjects controller CRUD functions

---
+++ app/Http/Controllers/SubjectController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Subject;
+use Illuminate\Http\Request;
+
+class SubjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $subjects = Subject::all();
+        return response()->json($subjects);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $subject = new Subject();
+        $subject->name= $request->name;
+
+        $subject->save();
+        return response()->json($subject);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $subject = Subject::find($id);
+        return response()->json($subject);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $subject= Subject::find($id);
+
+        $subject->name = $request->input('name');
+        $subject->save();
+        return response()->json($subject);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $subject = Subject::find($id);
+        $subject->delete();
+        return response()->json('The subject has been deleted successfully');
+    }
+}
