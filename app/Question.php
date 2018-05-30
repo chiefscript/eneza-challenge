1fbd49aff3f36dd76d482769d6d6f5706ee7d419
chiefscript <chiefscript@gmail.com>
2018-05-30 07:59:00 +0300

    Question model & migration

---
+++ app/Question.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Question extends Model
+{
+    use SoftDeletes;
+
+    protected $casts = [
+        'choices' => 'array'
+    ];
+
+    protected $fillable = ['question', 'choices'];
+}
