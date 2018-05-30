23124a4d19ce5bece08fbdc545fd73c09e34737e
chiefscript <>
2018-05-30 21:02:25 +0300

    Assign tutorial to subject

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Subject;
+use App\SubjectTutorial;
+use App\Tutorial;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Response as HttpResponse;
 
 class SubjectController extends Controller
 {
@@ -14,7 +18,7 @@ class SubjectController extends Controller
      */
     public function index()
     {
-        $subjects = Subject::all();
+        $subjects = Subject::with('tutorials')->get();
         return response()->json($subjects);
     }
 
@@ -71,5 +75,61 @@ class SubjectController extends Controller
         $subject = Subject::find($id);
         $subject->delete();
         return response()->json('The subject has been deleted successfully');
+    }
+
+    public function assignTutorial(Request $request)
+    {
+        $data = $request->only('course_id', 'subject_id');
+
+        $validator = Validator::make($request->all(), [
+            'subject_id' => 'required',
+            'tutorial_id' => 'required'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors()->all()
+            ], 422);
+        }
+
+        try {
+            $subject = Subject::find($request->subject_id);
+            $tutorial = Tutorial::find($request->tutorial_id);
+
+            if (!$subject) {
+                return response()->json([
+                    'message' => 'The subject selected does not exist',
+                    'status' => 0
+                ]);
+            }
+
+            if (!$tutorial) {
+                return response()->json([
+                    'message' => 'The tutorial selected does not exist',
+                    'status' => 0
+                ]);
+            }
+
+            $subject_tutorial = SubjectTutorial::firstOrNew([
+                'subject_id' => $request->subject_id,
+                'tutorial_id' => $request->tutorial_id
+            ]);
+
+            $subject_tutorial->fill($data);
+
+            $status = $subject_tutorial->save();
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'The record already exists.',
+                'status' => 0],
+                HttpResponse::HTTP_CONFLICT);
+        }
+
+        return response()->json([
+            'message' => 'The tutorial selected has been added to ' . $subject->name,
+            'status' => $status
+        ]);
     }
 }
